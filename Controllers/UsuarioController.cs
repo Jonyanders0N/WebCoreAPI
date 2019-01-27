10ec4bff2f8b54d5fefc19e66970efb121089118
RunF0rrestRun <sonyanderson19@gmail.com>
2019-01-26 22:25:46 -0300

    ...

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebCoreAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebCoreAPI.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioContext _context;
+
+        public UsuarioController(UsuarioContext context)
+        {
+            _context = context;
+
+            if (_context.Usuarios.Count() == 0)
+            {
+                _context.Add(new Usuario { NmUsuario = "Anderson" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET: api/<controller>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuario), new { id = usuario.UsuarioId }, usuario);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuario(long id, Usuario usuario)
+        {
+            if (id != usuario.UsuarioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUsuario(long id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreAPI.Models
+{
+    public class Usuario
+    {
+        public long UsuarioId { get; set; }
+        public string NmUsuario { get; set; }
+        public string senhaUsuario { get; set; }
+        public string emailUsuario { get; set; }
+    }
+}
